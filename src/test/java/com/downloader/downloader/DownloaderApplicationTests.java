639d929a415b54ce4ad85fee54f5517440a49868
workissing <workissing@163.com>
2022-12-02 19:17:19 +0800

    Initial commit

---
+++ src/test/java/com/downloader/downloader/DownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.downloader.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
